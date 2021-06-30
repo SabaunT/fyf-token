19e1e6e4c2aa7fa167d85aea262ad5f0a71f0a1f
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-06-30 08:24:48 +0300

    erc20detailed fns

--- contracts/SminemToken.sol
+++ contracts/SminemToken.sol
@@ -69,18 +69,40 @@ contract SminemToken is Context, Ownable, IERC20 {
         }
     }
 
+    // ERC20Detailed functions
+
+    /**
+     * @dev Returns the name of the token.
+     */
     function name() public view returns (string memory) {
         return _name;
     }
 
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
     function symbol() public view returns (string memory) {
         return _symbol;
     }
 
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
     function decimals() public view returns (uint8) {
         return _decimals;
     }
-
+    
+/*
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
@@ -324,5 +346,6 @@ contract SminemToken is Context, Ownable, IERC20 {
         }
         _reflectBurn(rAmount, tAmount, account);
     }
+*/
 }
 
