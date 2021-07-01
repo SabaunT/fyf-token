702c48a14d2c2a657933f4a82cade09151e9f219
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-07-01 12:04:41 +0300

    comments regargind development of the NFT

--- contracts/SminemERC721.sol
+++ contracts/SminemERC721.sol
@@ -1,8 +1,20 @@
 pragma solidity 0.5.7;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
-import "openzeppelin-solidity/contracts/drafts/Counters.sol";
+import "openzeppelin-solidity/contracts/access/roles/MinterRole.sol";
 
-contract SminemNFT is ERC721Full {
+contract SminemNFT is ERC721Full, MinterRole {
 
+    constructor() ERC721Full("SminemNFT", "SMNMNFT") public {}
+
+    function mint(address[] calldata to) external onlyMinter returns (uint256) {
+        /**
+          * Минт осуществляется с id, равным total supply
+          * Проверяешь какое количество токенов возможно сминтить исходя из счетчика на токене:
+          * - либо erc721 обращается в момент вызова этой функции к токену
+          * - либо токен сообщает, какое количество токенов можно сминтить контракту nft
+          * О том как устанавливать для новых токенов URI - смотри в новой реализации openzeppelin
+          */
+        return 1;
+    }
 }
